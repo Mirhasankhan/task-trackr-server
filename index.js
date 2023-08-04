9bb15ae87b29ba494cc80c2cf1c06cd2df52131c
Mirhasankhan <mirhasan000034@gmail.com>
2023-08-04 10:27:58 +0600

    api creation done

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+// middlewares
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cpvrkgd.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const taskCollection = client.db('taskDB').collection('task');
+
+    app.get('/tasks', async(req, res)=>{
+        let query = {};
+            if (req.query?.email) {
+                query = { email: req.query.email }
+            }
+            const result = await taskCollection.find(query).toArray()
+            res.send(result)
+    })
+
+    app.post('/tasks', async(req, res)=>{
+        const body = req.body
+        const result = await taskCollection.insertOne(body)
+        res.send(result)
+    })
+
+    app.patch('/tasks/:id', async(req, res)=>{
+        const id = req.params.id
+        const filter = {_id : new ObjectId(id)}
+        const updatedStatus = {
+            $set: {
+                status: 'Completed'
+            }
+        }
+        const result = await taskCollection.updateOne(filter,updatedStatus)
+        res.send(result)
+    })
+
+    app.delete('/tasks/:id', async(req, res)=>{
+        const id = req.params.id;
+        const filter = {_id : new ObjectId(id)}
+        const result = await taskCollection.deleteOne(filter)
+        res.send(result)
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('task trackr server running')
+})
+
+app.listen(port, () => {
+    console.log('server running at 5000');
+})
